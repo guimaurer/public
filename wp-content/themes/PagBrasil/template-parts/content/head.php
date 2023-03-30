299c509f23ac3f022f96c2562eed6418ca522642
guimaurer <guilhermemaurer.adm@gmail.com>
2023-03-30 04:05:56 -0300

    fix bug icon

--- wp-content/themes/PagBrasil/template-parts/content/head.php
+++ wp-content/themes/PagBrasil/template-parts/content/head.php
@@ -30,6 +30,8 @@
   <link href="<?php bloginfo('template_url') ?>/assets/css/style.css" rel="stylesheet">
   <link href="<?php bloginfo('stylesheet_url') ?>" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
